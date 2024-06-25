e01c40479d9e8c0527677554498c8d1ea4842288
Daniel Wiratman <daniel@equnix.biz>
2024-06-25 10:55:44 +0700

    BIN: remove install sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo install -m 755 youtube-music whatsapp-web whatsapp-web-work start_windows run_screenlayout sttmux /usr/bin
+sudo install -m 755 youtube-music whatsapp-web whatsapp-web-work start_windows run_screenlayout /usr/bin
